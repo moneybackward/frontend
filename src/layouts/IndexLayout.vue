0fb90554e30dbca659d1ee7bc166f9ed01b1d9b0
Jeremia Axel <jeremiaaxelb@gmail.com>
2023-11-28 21:57:20 +0700

    chore(layouts): move sidebar

---
+++ src/layouts/IndexLayout.vue
@@ -0,0 +1,38 @@
+<template>
+  <q-layout>
+    <q-header>
+      <q-toolbar>
+        <q-toolbar-title>
+          <q-avatar>
+            <img src="../assets/money_backward.png" alt="money backward logo" />
+          </q-avatar>
+          Money Backward
+        </q-toolbar-title>
+        <q-btn dense flat round icon="menu" @click="toggleRightDrawer" />
+      </q-toolbar>
+    </q-header>
+
+    <SidebarComponent ref="sidebarRef" />
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import { defineComponent, ref } from "vue";
+import SidebarComponent from "../components/Sidebar.vue";
+
+export default defineComponent({
+  name: "MainLayout",
+  components: {
+    SidebarComponent,
+  },
+  methods: {
+    toggleRightDrawer() {
+      this.$refs.sidebarRef.isOpen = !this.$refs.sidebarRef.isOpen;
+    },
+  },
+});
+</script>
--- src/router/routes.js
+++ src/router/routes.js
@@ -2,9 +2,27 @@
 const routes = [
   {
     path: '/',
-    component: () => import('layouts/MainLayout.vue'),
+    component: () => import('layouts/IndexLayout.vue'),
     children: [
       { path: '', component: () => import('pages/IndexPage.vue') }
+    ]
+  },
+  {
+    path: '/auth',
+    component: () => import('layouts/EmptyLayout.vue'),
+    children: [
+      { path: 'login', component: () => import('pages/auth/LoginPage.vue') },
+      { path: 'register', component: () => import('pages/auth/RegisterPage.vue') },
+      { path: 'logout', component: () => import('pages/auth/LogoutPage.vue') }
+    ]
+  },
+  {
+    path: '/app',
+    component: () => import('layouts/AppLayout.vue'),
+    children: [
+      { path: '', component: () => import('pages/DashboardPage.vue') },
+      { path: '', component: () => import('pages/DashboardPage.vue') },
+      { path: '', component: () => import('pages/DashboardPage.vue') }
     ]
   },
 
