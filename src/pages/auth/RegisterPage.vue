1f1d6abc1e15415b32cd8ec6237e048911897425
Ariya Adinatha <13519048@std.stei.itb.ac.id>
2023-11-30 01:27:31 +0700

    feat: authentication ui

--- src/pages/auth/RegisterPage.vue
+++ src/pages/auth/RegisterPage.vue
@@ -1,46 +1,75 @@
 <template>
-  <q-page class="col q-mx-md">
-    <h1>Register Page</h1>
-    <section>
-      <q-form @submit="onSubmit">
-        <q-input
-          filled
-          v-model="email"
-          label="Email"
-          :rules="emailRule"
-          lazy-rules
-        />
-        <q-input
-          filled
-          v-model="name"
-          label="Name"
-          :rules="nameRule"
-          lazy-rules
-        />
-        <q-input
-          filled
-          v-model="password"
-          label="Password"
-          :rules="passwordRule"
-          type="password"
-          lazy-rules
-        />
-        <q-input
-          filled
-          v-model="passwordConf"
-          label="Password Confirmation"
-          :rules="passwordConfRule"
-          type="password"
-          lazy-rules
-        />
-        <q-btn label="Register" type="submit" color="primary" />
-      </q-form>
-    </section>
-    <footer>
-      Already have an account?
-      <router-link to="/auth/login">Login</router-link>
-    </footer>
-  </q-page>
+  <q-page-container>
+    <q-page class="flex flex-center bg-grey-2">
+      <q-card class="q-pa-md shadow-2 my_card" bordered>
+        <q-card-section class="text-center">
+          <div class="text-grey-9 text-h5 text-weight-bold">Sign up</div>
+          <div class="text-grey-8">Sign in below to access your account</div>
+        </q-card-section>
+        <q-form @submit="onSubmit">
+          <q-card-section>
+            <q-input
+              dense
+              outlined
+              v-model="email"
+              label="Email Address"
+              :rules="emailRule"
+              lazy-rules
+            />
+            <q-input
+              dense
+              outlined
+              v-model="name"
+              label="Name"
+              :rules="nameRule"
+              lazy-rules
+            />
+            <q-input
+              dense
+              outlined
+              v-model="password"
+              label="Password"
+              type="password"
+              :rules="passwordRule"
+              lazy-rules
+            />
+            <q-input
+              dense
+              outlined
+              v-model="passwordConf"
+              label="Password Confirmation"
+              :rules="passwordConfRule"
+              type="password"
+              lazy-rules
+            />
+          </q-card-section>
+          <q-card-section>
+            <q-btn
+              style="border-radius: 8px"
+              color="dark"
+              rounded
+              size="md"
+              label="Sign up"
+              no-caps
+              class="full-width"
+              type="submit"
+            ></q-btn>
+          </q-card-section>
+        </q-form>
+        <q-card-section class="text-center q-pt-none">
+          <div class="text-grey-8">
+            Already have an account?
+            <a
+              href="/auth/login"
+              class="text-dark text-weight-bold"
+              style="text-decoration: none"
+              >Sign in.</a
+            >
+          </div>
+        </q-card-section>
+      </q-card>
+    </q-page>
+  </q-page-container>
 </template>
 
 <script setup lang="ts">
