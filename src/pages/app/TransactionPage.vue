8cebfdcee6aab9043b8ddfbaf36b49e43d118e28
Jeremia Axel <jeremiaaxelb@gmail.com>
2024-01-21 19:08:40 +0700

    smaller video preview size

--- src/pages/app/TransactionPage.vue
+++ src/pages/app/TransactionPage.vue
@@ -85,8 +85,8 @@
           <video
             ref="video"
             id="video"
-            width="640"
-            height="480"
+            width="320"
+            height="240"
             autoplay
           ></video>
         </q-card-section>
